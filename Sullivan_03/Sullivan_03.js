4bf434338a385b60dbf7180765ff0ada4dacdc80
Sullivan <kjsullivan1@labs.neit.edu>
2021-10-27 17:10:25 -0400

    END BIO 3

---
+++ Sullivan_03/Sullivan_03.js
@@ -0,0 +1,64 @@
+
+var store1 = "A libary"
+var store2 = "Your name" 
+var store3 = "A potatoe"     
+var store4 = "A needle"
+var store5 = "A clock"
+var store6 = "A coin"
+var store7 = "A deck of cards"
+var store8 = "A coat of paint"
+var store9 = "A cold"
+var store10 = "A fence" 
+
+
+
+function answer1(){
+
+    document.getElementById("question1").innerHTML = store1;
+}
+
+function answer2(){
+
+    document.getElementById("question2").innerHTML = store2;
+}
+
+function answer3(){
+
+    document.getElementById("question3").innerHTML = store3;
+}
+
+function answer4(){
+
+    document.getElementById("question4").innerHTML = store4;
+}
+
+function answer5(){
+
+    document.getElementById("question5").innerHTML = store5;
+
+}
+
+function answer6(){
+
+    document.getElementById("question6").innerHTML = store6;
+}
+
+function answer7(){
+
+    document.getElementById("question7").innerHTML = store7;
+}
+
+function answer8(){
+
+    document.getElementById("question8").innerHTML = store8;
+}
+
+function answer9(){
+
+    document.getElementById("question9").innerHTML = store9;
+}
+
+function answer10(){
+
+    document.getElementById("question10").innerHTML = store10;
+}
