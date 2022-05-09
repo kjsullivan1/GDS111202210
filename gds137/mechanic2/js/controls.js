32717835b4949a255ebe6a665463209a09a19e1c
Keegan Sullivan <kjsullivan1@email.neit.edu>
2022-05-09 13:40:07 -0400

    lol

---
+++ gds137/mechanic2/js/controls.js
@@ -0,0 +1,71 @@
+var w = false;
+var a = false;
+var s = false;
+var d = false;
+var space = false;
+var f = false;
+
+document.addEventListener("keydown", press);
+document.addEventListener("keyup", release);
+
+function press(e)
+{
+	//---This logs key codes into the browser's console.
+	console.log(e.keyCode);
+	
+	if(e.keyCode == 87)
+	{
+		w = true;
+	}
+	if(e.keyCode == 65)
+	{
+		a = true;
+	}
+	if(e.keyCode == 83)
+	{
+		s = true;
+	}
+	if(e.keyCode == 68)
+	{
+		d = true;
+	}
+	if(e.keyCode == 32)
+	{
+		space = true;
+	}
+	if(e.keyCode == 70)
+	{
+		f = true;
+	}
+}
+
+function release(e)
+{
+	//---This logs key codes into the browser's console.
+	//console.log(e.keyCode);
+	
+	if(e.keyCode == 87)
+	{
+		w = false;
+	}
+	if(e.keyCode == 65)
+	{
+		a = false;
+	}
+	if(e.keyCode == 83)
+	{
+		s = false;
+	}
+	if(e.keyCode == 68)
+	{
+		d = false;
+	}
+	if(e.keyCode == 32)
+	{
+		space = false;
+	}
+	if(e.keyCode == 70)
+	{
+		f = false;
+	}
+}
