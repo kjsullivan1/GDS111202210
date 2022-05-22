7f938ef9d1c5fb6b962aa6dbbf51de6b13139202
Keegan Sullivan <kjsullivan1@email.neit.edu>
2022-05-22 14:06:48 -0400

    lol

---
+++ gds137/Level4_Bigboss/js/physics.js
@@ -0,0 +1,57 @@
+var frictionX = .98;	
+var frictionY = .97;
+var gravity = 1;
+
+
+
+
+function showGravity()
+{
+	
+	
+	
+	//--------------Apply Gravity to the Velocity Y-----------------------------------------
+	ball.vy += gravity;
+	ball.y += ball.vy;
+	//---------------------------------------------------------------------------------------
+	
+	ball.vx *= frictionX;
+	ball.x += ball.vx;
+}
+
+function showBounce()
+{
+	
+	
+	
+	//--------------------Check Collision------------------------------------------------------
+	if(ball.y > canvas.height - ball.height/2)
+	{
+		
+		//--------Bounce the Ball---------------------------------------------------------------
+		ball.y = canvas.height - ball.height/2;
+		//the decimal is how bouncy you want the object to be
+		//It should be a number between 0 and 2;
+		ball.vy = -ball.vy * .67;
+        score = 0
+	}
+	
+	//-----------------------------------------------------------------------------------------
+}
+
+function showAcceleration()
+{
+	//--------------Use Velocity and Acceleration to move around----------------------
+	if(d)
+	{	
+		player.vx +=  player.ax * player.force;
+	}
+	if(a)
+	{
+		player.vx += player.ax * -player.force;
+	}
+
+	//---------------------------------------------------------------------------------------
+	//player.x += player.vx;
+	//player.y += player.vy;
+}
